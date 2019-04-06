13d4118f06f6601ceb8c220f45adaaf5b1199fc2
Titir Adhikary <titir.adhikary@hotmail.com>
2019-04-06 09:53:47 +0530

    first commit

---
+++ friend.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int testcases;
+    scanf("%d", &testcases);
+    while(testcases-- != 0)
+    {
+        long int n;
+        scanf("%ld", &n);
+        char *string = (char*)malloc((n + 1) * sizeof(char));
+        scanf("%s", string);
+        char ch;
+        scanf(" %c", &ch);
+        long int count = 0;
+        long int val = n*(n + 1)/2; 
+        for(int i = 0 ; i < n ; i++)
+        {
+            //printf("%c", ch);
+            if(string[i] != ch)
+                count++;
+            else
+            {
+                //printf("%ld\n", count);
+                val = val - count*(count + 1)/2;
+                count = 0;
+            }
+            
+        }
+        printf("%ld\n", val);
+    }
+}
